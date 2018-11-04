72c03d76f92cc6d6ff8c6fac147c872e5969732e
Kyle Knaggs <kyle@adfoam.com>
2018-11-04 19:26:35 -0400

    Prettier formats tinytest.js on save

--- tinytest.js
+++ tinytest.js
@@ -38,54 +38,61 @@
  * MIT License. See https://github.com/joewalnes/jstinytest/
  */
 var TinyTest = {
+  run: function(tests) {
+    var failures = 0;
+    for (var testName in tests) {
+      var testAction = tests[testName];
+      try {
+        testAction.apply(this);
+        console.log("Test:", testName, "OK");
+      } catch (e) {
+        failures++;
+        console.error("Test:", testName, "FAILED", e);
+        console.error(e.stack);
+      }
+    }
+    setTimeout(function() {
+      // Give document a chance to complete
+      if (window.document && document.body) {
+        document.body.style.backgroundColor =
+          failures == 0 ? "#99ff99" : "#ff9999";
+      }
+    }, 0);
 
-    run: function(tests) {
-        var failures = 0;
-        for (var testName in tests) {
-            var testAction = tests[testName];
-            try {
-                testAction.apply(this);
-                console.log('Test:', testName, 'OK');
-            } catch (e) {
-                failures++;
-                console.error('Test:', testName, 'FAILED', e);
-                console.error(e.stack);
-            }
-        }
-        setTimeout(function() { // Give document a chance to complete
-            if (window.document && document.body) {
-                document.body.style.backgroundColor = (failures == 0 ? '#99ff99' : '#ff9999');
-            }
-        }, 0);
-    },
+    // Browser run order:
+    // 1. Javascript
+    // 2. Update the DOM
+    // 3. Extra tasks (e.g. callbacks passed into setTimeout() etc.)
+  },
 
-    fail: function(msg) {
-        throw new Error('fail(): ' + msg);
-    },
+  fail: function(msg) {
+    throw new Error("fail(): " + msg);
+  },
 
-    assert: function(value, msg) {
-        if (!value) {
-            throw new Error('assert(): ' + msg);
-        }
-    },
+  assert: function(value, msg) {
+    if (!value) {
+      throw new Error("assert(): " + msg);
+    }
+  },
 
-    assertEquals: function(expected, actual) {
-        if (expected != actual) {
-            throw new Error('assertEquals() "' + expected + '" != "' + actual + '"');
-        }
-    },
+  assertEquals: function(expected, actual) {
+    if (expected != actual) {
+      throw new Error('assertEquals() "' + expected + '" != "' + actual + '"');
+    }
+  },
 
-    assertStrictEquals: function(expected, actual) {
-        if (expected !== actual) {
-            throw new Error('assertStrictEquals() "' + expected + '" !== "' + actual + '"');
-        }
-    },
-
+  assertStrictEquals: function(expected, actual) {
+    if (expected !== actual) {
+      throw new Error(
+        'assertStrictEquals() "' + expected + '" !== "' + actual + '"'
+      );
+    }
+  }
 };
 
-var fail               = TinyTest.fail.bind(TinyTest),
-    assert             = TinyTest.assert.bind(TinyTest),
-    assertEquals       = TinyTest.assertEquals.bind(TinyTest),
-    eq                 = TinyTest.assertEquals.bind(TinyTest), // alias for assertEquals
-    assertStrictEquals = TinyTest.assertStrictEquals.bind(TinyTest),
-    tests              = TinyTest.run.bind(TinyTest);
+var fail = TinyTest.fail.bind(TinyTest),
+  assert = TinyTest.assert.bind(TinyTest),
+  assertEquals = TinyTest.assertEquals.bind(TinyTest),
+  eq = TinyTest.assertEquals.bind(TinyTest), // alias for assertEquals
+  assertStrictEquals = TinyTest.assertStrictEquals.bind(TinyTest),
+  tests = TinyTest.run.bind(TinyTest);
